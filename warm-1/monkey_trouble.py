33f728fe2055530c6e62abbe02ec38213aec1a3a
samanthaalcantara <65324260+samanthaalcantara@users.noreply.github.com>
2020-05-29 19:49:39 -0500

    Update monkey_trouble.py

--- warm-1/monkey_trouble.py
+++ warm-1/monkey_trouble.py
@@ -4,3 +4,9 @@ def monkey_trouble(a_smile, b_smile):
   if not a_smile and not b_smile:
     return True
   return False
+
+def monkey_trouble(a_smile, b_smile):
+  if((a_smile and b_smile) or ((not a_smile) and (not b_smile)):
+     return True
+   else:
+     return False
