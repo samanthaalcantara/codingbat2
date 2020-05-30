e33a7ca68fe4b2b51132104809eb53840dcf0ade
samanthaalcantara <65324260+samanthaalcantara@users.noreply.github.com>
2020-05-29 19:52:46 -0500

    Update parrot_trouble.py

--- warm-1/parrot_trouble.py
+++ warm-1/parrot_trouble.py
@@ -1,2 +1,11 @@
 def parrot_trouble(talking, hour):
   return (talking and (hour < 7 or hour > 20))
+
+def parrot_trouble(talking, hour):
+  if talking:
+    if hour<7 or hour<20:
+      return True
+    else:
+      return False
+    else:
+      return False
