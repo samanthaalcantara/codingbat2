fb4c4b8da61cc3744372e9791d299c3aa87bc249
samanthaalcantara <samanthaalcantara28@gmail.com>
2020-06-07 18:50:48 -0500

    add front_times to my directory

---
+++ warm-2/front_times.py
@@ -0,0 +1,11 @@
+""
+Date: 06 07 2020
+Author: Samantha Alcantara
+"""
+Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+
+def front_times(str, n):
+  if len(str) < 3:
+    return str * n
+  return str[:3] * n
+
