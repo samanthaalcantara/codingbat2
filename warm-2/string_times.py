82b71468ee88ee74a8b25410837f4f3c546deb63
samanthaalcantara <samanthaalcantara28@gmail.com>
2020-06-07 18:43:59 -0500

    add string_times to my directory

---
+++ warm-2/string_times.py
@@ -0,0 +1,18 @@
+"""
+Author: Sam
+Date: June 07, 2020
+Question:
+Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+
+string_times('Hi', 2) → 'HiHi'
+string_times('Hi', 3) → 'HiHiHi'
+string_times('Hi', 1) → 'Hi'
+"""
+#Answer 
+def string_times(str, n):
+  result = ""
+ for i in range(n): 
+     range(n) is [0, 1, 2, .... n-1]
+result = result + str
+ return result
