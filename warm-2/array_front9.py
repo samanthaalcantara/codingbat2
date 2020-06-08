49a4f1faa76e1af2993ad5bd82925750d0699483
samanthaalcantara <samanthaalcantara28@gmail.com>
2020-06-07 19:23:09 -0500

    add array_front9 to my directory

---
+++ warm-2/array_front9.py
@@ -0,0 +1,11 @@
+""""
+Date: 06 07 2020
+Author: Samantha Alcantara
+Question: Given an array of ints, return True if one of the first 4 elements in the array is a 9. The array length may be less than 4.
+
+def array_front9(nums):
+ for element in nums [:4]:
+  if element == 9:
+    return True
+    return False
+
