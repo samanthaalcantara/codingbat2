34923656d6f6c0dc38445400b1ed973bb7c35472
samanthaalcantara <samanthaalcantara28@gmail.com>
2020-06-07 19:13:51 -0500

    add array_count9 to my file directory

---
+++ warm-2/array_count9.py
@@ -0,0 +1,10 @@
+""""
+Date: 06 07 2020
+Author: Samantha Alcantara
+Question: Given an array of ints, return the number of 9's in the array.
+
+def array_count9(nums):
+ for element in nums[:4]:
+  if element == 9:
+     return True
+      return False
