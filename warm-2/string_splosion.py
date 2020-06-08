6a2b3f3b561c13f4b413bf5cde21838a81b68fb4
samanthaalcantara <samanthaalcantara28@gmail.com>
2020-06-07 19:03:14 -0500

    add string_splosion to my file directory

---
+++ warm-2/string_splosion.py
@@ -0,0 +1,11 @@
+""""
+Date: 06 07 2020
+Author: Samantha Alcantara
+Question: Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+def string_splosion(str):
+  result = ‘’
+   for i in range(0,len(str)+1):
+    result += str[:n]
+   return result
+
