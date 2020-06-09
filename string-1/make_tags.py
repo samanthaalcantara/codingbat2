d4103f8c6e0fd307044576dc3f319c4008579f32
samanthaalcantara <samanthaalcantara28@gmail.com>
2020-06-08 21:43:07 -0500

    add hello_name to my file directory.

---
+++ string-1/make_tags.py
@@ -0,0 +1,10 @@
+"""
+Date: 06 07 2020
+Author: Samantha Alcantara
+Question: The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+"""
+
+Answer: 
+def make_tags(tag, word):
+  return "<" + tag + ">" + word + "</" + tag + ">"
+
