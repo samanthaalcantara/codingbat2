10f0f5b63b603db02c88794d42a9238e58e3e2d9
samanthaalcantara <samanthaalcantara28@gmail.com>
2020-06-09 01:25:51 -0500

    add has23 to my file directory.

---
+++ list-1/has23.py
@@ -0,0 +1,10 @@
+"""
+Date: 06 08 2020
+Author: Samantha Alcantara
+Question: Given an int array length 2, return True if it contains a 2 or a 3.
+"""
+
+#Answer
+
+def has23(nums):
+  return 2 in nums or 3 in nums
