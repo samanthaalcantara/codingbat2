edea87ba87829b9f6c3835701585c46c64e7f922
samanthaalcantara <samanthaalcantara28@gmail.com>
2020-06-09 01:10:44 -0500

    add middle_way to my file directory.

---
+++ list-1/middle_way.py
@@ -0,0 +1,10 @@
+"""
+Date: 06 08 2020
+Author: Samantha Alcantara
+Question: Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+"""
+
+#Answer
+
+def middle_way(a, b):
+  return [a[1], b[1]]
