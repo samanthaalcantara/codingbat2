97945b525a51f71f1cdd880ab85fcba9198f8a6d
samanthaalcantara <samanthaalcantara28@gmail.com>
2020-06-08 23:37:01 -0500

    add make_pi to my file directory.

---
+++ list-1/make_pi.py
@@ -0,0 +1,11 @@
+"""
+Date: 06 08 2020
+Author: Samantha Alcantara
+Question: Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+"""
+
+#Answer
+
+def make_pi():
+  return [3, 1, 4]
+
