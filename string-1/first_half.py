79c480d3bdee42464c5ecbd4d5802ee79c05b78d
samanthaalcantara <samanthaalcantara28@gmail.com>
2020-06-08 21:53:30 -0500

    add first_half to my file directory.

---
+++ string-1/first_half.py
@@ -0,0 +1,11 @@
+"""
+Date: 06 08 2020
+Author: Samantha Alcantara
+Question:Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+"""
+
+Answer:
+
+def first_half(str):
+  return str[:len(str)/2] 
+ 
