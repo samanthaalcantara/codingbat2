a3553a8212e275086f8ca90116d027055c7271cd
samanthaalcantara <samanthaalcantara28@gmail.com>
2020-06-09 01:20:10 -0500

    add make_ends to my file directory.

---
+++ list-1/make_ends.py
@@ -0,0 +1,11 @@
+"""
+Date: 06 08 2020
+Author: Samantha Alcantara
+Question: 
+Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more."""
+
+#Answer
+
+def make_ends(nums):
+  return [nums[0], nums[-1]]
+
