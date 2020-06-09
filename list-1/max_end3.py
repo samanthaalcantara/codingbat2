55939c53ca777cf24d6ac43909631e2c820521b3
samanthaalcantara <samanthaalcantara28@gmail.com>
2020-06-09 00:49:31 -0500

    add max_end3 to my file directory.

---
+++ list-1/max_end3.py
@@ -0,0 +1,13 @@
+"""
+Date: 06 08 2020
+Author: Samantha Alcantara
+Question: Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value. Return the changed array.
+"""
+
+#Answer
+
+def max_end3(nums):
+  m = max(nums[0], nums[2])
+  return [m, m, m]
+
+
