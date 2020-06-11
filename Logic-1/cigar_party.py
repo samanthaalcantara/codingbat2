e43683a29990d91f9409ea3b464f2c6b632adc26
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-06-10 21:50:05 -0500

    Update cigar_party.py

--- Logic-1/cigar_party.py
+++ Logic-1/cigar_party.py
@@ -1,7 +1,10 @@
 """
 Date: 06 10 2020
 Author: Samantha Alcantara
-Question: When squirrels get together for a party, they like to have cigars. A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. Unless it is the weekend, in which case there is no upper bound on the number of cigars. Return True if the party with the given values is successful, or False otherwise.
+Question: When squirrels get together for a party, they like to have cigars. 
+A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. 
+Unless it is the weekend, in which case there is no upper bound on the number of cigars. 
+Return True if the party with the given values is successful, or False otherwise.
 """
 
 #Answer
