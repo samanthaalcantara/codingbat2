6d7d548b6b1e2f63a3487b3487f4b4b7875028c2
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-06-10 21:48:39 -0500

    Update date_fashion.py

--- Logic-1/date_fashion.py
+++ Logic-1/date_fashion.py
@@ -8,10 +8,4 @@ You and your date are trying to get a table at a restaurant. The parameter "you"
 
 #Answer
 
-def date_fashion(you, date):
-  if you <= 2 or date <= 2:
-    return 0
-  if you >= 8 or date >= 8:
-    return 2
-  return 1
-  
+
