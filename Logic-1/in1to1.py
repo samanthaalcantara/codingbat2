09ae83cdbed608cd8610324db652b4ae8ec709b6
samanthaalcantara <samanthaalcantara28@gmail.com>
2020-06-14 02:24:55 -0500

    add in1to1 to my file directory.

---
+++ Logic-1/in1to1.py
@@ -0,0 +1,15 @@
+"""
+Date: 06 12 2020
+Author: Samantha Alcantara
+Question: 
+Given a number n, return True if n is in the range 1..10, inclusive. Unless outside_mode is True, in which case return True if the number is less or equal to 1, or greater or equal to 10.
+"""
+
+
+#Answer
+
+def in1to10(n, outside_mode):
+  if outside_mode:
+    return n <= 1 or n >= 10
+  return 1 <=  n <= 10
+
