4942490deed2bb3b3effd253df98932efa3f54b0
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-06-14 19:30:26 -0500

    Update alarm_clock.py

--- Logic-1/alarm_clock.py
+++ Logic-1/alarm_clock.py
@@ -2,7 +2,12 @@
 Date: 06 12 2020
 Author: Samantha Alcantara
 Question: 
-Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating if we are on vacation, return a string of the form "7:00" indicating when the alarm clock should ring. Weekdays, the alarm should be "7:00" and on the weekend it should be "10:00". Unless we are on vacation -- then on weekdays it should be "10:00" and weekends it should be "off".
+Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, 
+and a boolean indicating if we are on vacation, return a string of 
+the form "7:00" indicating when the alarm clock should ring. Weekdays, 
+the alarm should be "7:00" and on the weekend it should be "10:00". 
+Unless we are on vacation -- then on weekdays it should be "10:00" and 
+weekends it should be "off".
 """
 
 
