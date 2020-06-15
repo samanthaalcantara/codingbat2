9f0fe0e44b79db9423f676ee45ef7f5ae5f9a668
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-06-14 19:31:25 -0500

    Update love6.py

--- Logic-1/love6.py
+++ Logic-1/love6.py
@@ -1,7 +1,10 @@
 """
 Date: 06 12 2020
 Author: Samantha Alcantara
-Question: The number 6 is a truly great number. Given two int values, a and b, return True if either one is 6. Or if their sum or difference is 6. Note: the function abs(num) computes the absolute value of a number
+Question: The number 6 is a truly great number. 
+Given two int values, a and b, return True if 
+either one is 6. Or if their sum or difference is 6. 
+Note: the function abs(num) computes the absolute value of a number
 """
 
 
