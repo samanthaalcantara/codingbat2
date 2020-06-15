0a8e02ef80a942299bc34f6454d43ab7ebfe41e8
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-06-14 20:07:58 -0500

    Update middle_way.py

--- list-1/middle_way.py
+++ list-1/middle_way.py
@@ -1,7 +1,8 @@
 """
 Date: 06 08 2020
 Author: Samantha Alcantara
-Question: Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+Question: Given 2 int arrays, a and b, each length 3, 
+return a new array length 2 containing their middle elements.
 """
 
 #Answer
