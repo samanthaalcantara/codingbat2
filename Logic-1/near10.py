17af7d3b87d5ee1f6b1ffa191a6b28bfddd74163
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-06-14 19:31:46 -0500

    Update near10.py

--- Logic-1/near10.py
+++ Logic-1/near10.py
@@ -2,7 +2,9 @@
 Date: 06 12 2020
 Author: Samantha Alcantara
 Question: 
-Given a non-negative number "num", return True if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2.
+Given a non-negative number "num", 
+return True if num is within 2 of a multiple of 10. 
+Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2.
 """
 
 
