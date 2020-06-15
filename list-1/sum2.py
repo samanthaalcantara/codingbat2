63bbb190d26a9f7708a4a68aa6fd5f3388d0c09b
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-06-14 20:09:18 -0500

    Update sum2.py

--- list-1/sum2.py
+++ list-1/sum2.py
@@ -1,7 +1,10 @@
 """
 Date: 06 08 2020
 Author: Samantha Alcantara
-Question: Given an array of ints, return the sum of the first 2 elements in the array. If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+Question: Given an array of ints, 
+return the sum of the first 2 elements in the array. 
+If the array length is less than 2, just sum up the elements 
+that exist, returning 0 if the array is length 0.
 """
 
 #Answer
