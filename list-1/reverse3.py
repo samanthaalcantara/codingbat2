7b546fc704b634923cdcb87556dfe57322116713
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-06-14 20:08:22 -0500

    Update reverse3.py

--- list-1/reverse3.py
+++ list-1/reverse3.py
@@ -1,7 +1,8 @@
 """
 Date: 06 08 2020
 Author: Samantha Alcantara
-Question: Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
+Question: Given an array of ints length 3, 
+return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
 """
 
 #Answer
