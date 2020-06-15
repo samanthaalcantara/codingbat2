08443a2066f6969c40f1d305a74306c47a9484a1
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-06-14 19:32:31 -0500

    Update squirrel_play.py

--- Logic-1/squirrel_play.py
+++ Logic-1/squirrel_play.py
@@ -1,7 +1,12 @@
 """
 Date: 06 12 2020
 Author: Samantha Alcantara
-Question: The squirrels in Palo Alto spend most of the day playing. In particular, they play if the temperature is between 60 and 90 (inclusive). Unless it is summer, then the upper limit is 100 instead of 90. Given an int temperature and a boolean is_summer, return True if the squirrels play and False otherwise.
+Question: The squirrels in Palo Alto 
+spend most of the day playing. In particular, 
+they play if the temperature is between 60 and 90 (inclusive). 
+Unless it is summer, then the upper limit is 100 instead of 90. 
+Given an int temperature and a boolean is_summer, return True if 
+the squirrels play and False otherwise.
 """
 
 
