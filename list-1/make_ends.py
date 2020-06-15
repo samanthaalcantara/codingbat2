c0057f2711a2b15c03b778cbea0a752bf95d844a
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-06-14 20:07:01 -0500

    Update make_ends.py

--- list-1/make_ends.py
+++ list-1/make_ends.py
@@ -2,7 +2,10 @@
 Date: 06 08 2020
 Author: Samantha Alcantara
 Question: 
-Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more."""
+Given an array of ints, return a new array length 2 
+containing the first and last elements from the original array. 
+The original array will be length 1 or more.
+"""
 
 #Answer
 
