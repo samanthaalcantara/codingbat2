077317d4eb7ef2f31a498525f8332eaef650f56a
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-06-14 20:08:41 -0500

    Update rotate_left3.py

--- list-1/rotate_left3.py
+++ list-1/rotate_left3.py
@@ -1,7 +1,8 @@
 """
 Date: 06 08 2020
 Author: Samantha Alcantara
-Question: Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+Question: Given an array of ints length 3,
+return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
 """
 
 #Answer
