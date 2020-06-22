7f053b57e6d2e18122ce88798c0913e90cb35119
samanthaalcantara <samanthaalcantara28@gmail.com>
2020-06-22 16:41:26 -0500

    add sum67

---
+++ sum67.py
@@ -0,0 +1,11 @@
+"""
+Date: 06 22 2020
+Author: Samantha Alcantara
+Question: 
+Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers.
+
+
+#Answer
+
+
+
