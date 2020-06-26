b98b2fd47793415ab3f56bd259c2c4258d62e247
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-06-26 14:56:59 -0500

    Update sum13.py

--- list-2/sum13.py
+++ list-2/sum13.py
@@ -1,7 +1,9 @@
 """
 Date: 06 22 2020
 Author: Samantha Alcantara
-Question: Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
+Question: Return the sum of the numbers in the array, r
+eturning 0 for an empty array. Except the number 13 is very unlucky, 
+so it does not count and numbers that come immediately after a 13 also do not count.
 """
 
 
