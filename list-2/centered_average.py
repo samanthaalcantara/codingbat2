070776f4f8bb822ca11f929aaca21335885fc9ac
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-06-26 14:54:56 -0500

    Update centered_average.py

--- list-2/centered_average.py
+++ list-2/centered_average.py
@@ -2,7 +2,12 @@
 Date: 06 22 2020
 Author: Samantha Alcantara
 Question: 
-Return the "centered" average of an array of ints, which we'll say is the mean average of the values, except ignoring the largest and smallest values in the array. If there are multiple copies of the smallest value, ignore just one copy, and likewise for the largest value. Use int division to produce the final average. You may assume that the array is length 3 or more.
+Return the "centered" average of an array of ints, 
+which we'll say is the mean average of the values, 
+except ignoring the largest and smallest values in the array. 
+If there are multiple copies of the smallest value, ignore just one copy, 
+and likewise for the largest value. Use int division to produce the final average. 
+You may assume that the array is length 3 or more.
 """
 
 
