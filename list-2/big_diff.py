901ddfe31f3ac61d22700b0c3f4713ff239fac02
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-06-26 14:54:18 -0500

    Update big_diff.py

--- list-2/big_diff.py
+++ list-2/big_diff.py
@@ -2,7 +2,9 @@
 Date: 06 22 2020
 Author: Samantha Alcantara
 Question: 
-Given an array length 1 or more of ints, return the difference between the largest and smallest values in the array. Note: the built-in min(v1, v2) and max(v1, v2) functions return the smaller or larger of two values.
+Given an array length 1 or more of ints, return the difference 
+between the largest and smallest values in the array. Note: the built-in min(v1, v2) 
+and max(v1, v2) functions return the smaller or larger of two values.
 """
 
 
