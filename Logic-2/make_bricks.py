b7783f7eaa2d306f24ee29ae30ba1d08d64ac582
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-06-26 14:31:40 -0500

    Update make_bricks.py

--- Logic-2/make_bricks.py
+++ Logic-2/make_bricks.py
@@ -2,7 +2,10 @@
 Date: 06 13 2020
 Author: Samantha Alcantara
 Question: 
-We want to make a row of bricks that is goal inches long. We have a number of small bricks (1 inch each) and big bricks (5 inches each). Return True if it is possible to make the goal by choosing from the given bricks. This is a little harder than it looks and can be done without any loops.
+We want to make a row of bricks that is goal inches long. 
+We have a number of small bricks (1 inch each) and big bricks (5 inches each). 
+Return True if it is possible to make the goal by choosing from the given bricks. 
+This is a little harder than it looks and can be done without any loops.
 """
 
 #Answer
